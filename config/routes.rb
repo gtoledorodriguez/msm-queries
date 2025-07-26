0bdb7351957bf6a9ffa281612f22774899ba115a
Gladys Toledo-Rodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2025-07-26 05:36:42 +0000

    Adds movie#index and movie#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
+
   get("/directors", {controller: "directors", action: "index"})
+
   get("/directors/youngest", { :controller => "directors", :action => "youngest" })
+
   get("/directors/eldest", { :controller => "directors", :action => "eldest" })
+  
   get("/directors/:the_id", {controller: "directors", action: "show"})
+
+  get("/movies", {controller: "movies", action: "index"})
+  
+  get("/movies/:the_id", {controller: "movies", action: "show"})
+
 end
