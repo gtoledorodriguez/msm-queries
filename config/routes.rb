390edd7d9e103e330a6b61fd6923058931051d4a
Gladys Toledo-Rodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2025-07-26 05:14:54 +0000

    Adds directors#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
   get("/directors", {controller: "directors", action: "index"})
+  get("/directors/youngest", { :controller => "directors", :action => "youngest" })
+  get("/directors/eldest", { :controller => "directors", :action => "eldest" })
+  get("/directors/:the_id", {controller: "directors", action: "show"})
 end
